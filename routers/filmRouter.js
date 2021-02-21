1f357ccec137ac7fc5d4fbb562c476b791d8fa37
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-02-21 18:57:12 +0100

    Archivo movieRouter 'CREADO' Añadidas API ROUTES.

---
+++ routers/filmRouter.js
@@ -0,0 +1,64 @@
+const routerFilms = require('express').Router();
+const filmController = require('../controllers/filmController')
+const filmSchema = require('../models/film')
+
+// API routes
+routerFilms.get('/films', async (req, res) => {
+    try {
+        res.json(await filmController.findAllFilms())
+    }catch (err) {
+        return res.sendStatus(500).json({
+           message: 'Internal Server Error'
+        });
+        console.log(err.message)
+    }
+});
+
+routerFilms.get('/film:id',async (req, res) => {
+    try {
+        res.json(await filmController.findOne(req))
+    }catch (err) {
+        return res.sendStatus(500).json({
+            message: 'Internal Server Error'
+        });
+    }
+});
+
+routerFilms.post('/add-film',async (req, res) => {
+    try{
+        const id = await filmController.addFilm(new filmSchema(req.body));
+        const status = 'success';
+        res.json({status,id});
+    } catch( error ){
+        return res.sendStatus(500).json({
+            message: 'Internal Server Error'
+        });
+    }
+})
+
+routerFilms.put('/update-film:id',async (req,res) => {
+    try{
+        const id = req.params.id;
+        res.json(await filmController.updateFilm(id,new filmSchema(req.body)));
+    } catch( error ){
+        return res.sendStatus(500).json({
+            message: 'Internal Server Error'
+        });
+    }
+});
+
+routerFilms.delete('/remove-film/:id', async (req, res) => {
+    try{
+        const id = req.params.id;
+        const status = 'deleted'
+        await filmController.deleteFilm(id);
+        res.json({status,id});
+    } catch( error ) {
+        return res.sendStatus(500).json({
+            message: 'Internal Server Error'
+     });   
+    }
+ 
+ });
+
+module.exports = routerFilms;
