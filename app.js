f18878ebb49c72dd251a8ef9c63acf8d119a6210
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-02-21 18:48:49 +0100

    Archivo model/film.js 'MODIFICADO' Eliminada clase film  anterior. Añadida clase filmSchema.

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const app = express()
 const db = require('./db');
 const port = 3000;
 
-// Escucha del puerto 3000 en la consola con el comando 'npm start' en la Terminal.
 
-app.listen(port, () => console.log(`Listening at ${port}`));
+
+// Middleware
+app.use(express.json())
+
+app.use(routerFilms);
+
+
+// Start server // Escucha del puerto 3000 en la consola con el comando 'npm start' en la Terminal.
+db
+.then(() => {
+    app.listen(port, () => console.log(`Node server running on http://localhost:${port}`));
+})
+.catch((err) => console.log(err.message));
--- models/film.js
+++ models/film.js
@@ -1,18 +1,61 @@
-class Film {
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
 
-    constructor(id, title, year, description, image, genre, adult) {
-        this.title = title,
-        this.id = id,
-        this.year = year,
-        this.description = description,
-        this.image = image,
-        this.genre = genre,
-        this.adult = adult
+const filmSchema = new Schema({
+    id: { 
+        type: ObjectId,
+    },
+    title: { 
+        type: String,
+        required: true,
+        deafault: 'film'
+    },
+    creationDate: {
+        type: Date,
+        default: new Date
+    },
+    year: { 
+        type: Number 
+    },
+    country: { 
+        type: String 
+    },
+    poster: { 
+        type: String 
+    },
+    genre: { 
+        type: String, 
+        enum:
+        {
+            "Action": 28,
+            "Adventure": 12,
+            "Animation": 16,
+            "Comedy": 35,
+            "Crime": 80,
+            "Documentary": 99,
+            "Drama": 18,
+            "Family": 10751,
+            "Fantasy": 14,
+            "History": 36,
+            "Horror": 27,
+            "Music": 10402,
+            "Mystery": 9648,
+            "Romance":10749,
+            "Science Fiction":878,
+            "Thriller":53,
+            "War": 10752,
+            "Western": 37
+        }
+    },
+    description: { 
+        type: String 
+    },
+    adult: { 
+        type: Boolean,
+        default: false
     }
+})
 
-    async takeMovies () {
-
-    }
-}
-
-module.exports = Film;
+const film = mongoose.model('film', filmSchema);
+module.exports = film;
