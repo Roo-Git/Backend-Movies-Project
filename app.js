825dba70530fb759528be8786105057dc1feaf81
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-02-23 18:53:44 +0100

    Archivos: orderController.js, order.js(model) y orderRouter.js 'CREADOS'. App.js routerOrder (middleware) 'CREADO'.

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express()
 const db = require('./db');
 const port = 3000;
-const routerFilms = require('./routes/filmRouter')
-const routerUsers = require('./routes/userRouter')
+const routerFilms = require('./routes/filmRouter');
+const routerUsers = require('./routes/userRouter');
+const routerOrder = require('./routes/orderRouter');
 
 
 // Middleware
 app.use(express.json())
 app.use(routerFilms);    
-app.use(routerUsers)
+app.use(routerUsers);
+app.use(routerOrder);
 
 
 // Start server // Escucha del puerto 3000 en la consola con el comando 'npm start' en la Terminal.
