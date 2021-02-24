5e3f0b2ba0c21d4cc23e3582ee6466d012859047
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-02-24 18:51:24 +0100

    Order Controller Post y Get planteados.

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,1 +1,26 @@
+const order = require('../models/order');
 
+class Rental {
+
+    constructor(){
+
+    }
+
+    //POST - Create a new Order 
+
+    async createOrder(rental){
+        return order.create(rental)
+    };
+
+
+    //GET - Return all Rentals in the DB
+
+    async findAllOrders(){
+        return Order.find();
+    };
+
+
+}
+
+let orderController = new Rental();
+module.exports = orderController;
